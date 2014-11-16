132345837596d3eccb1dc0f4f83dff067b22cfcf
Ian Offerdahl <ianofferdahl@gmail.com>
2014-11-16 01:37:07 -0600

    Pretty Much Finished

---
+++ FileNaming/FileNameUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNaming
+{
+    public class FileNameUtility
+    {
+
+
+        public int FindDate(string input)
+        {
+            var temp = System.Text.RegularExpressions.Regex.Match(input, @"\d{4}");
+            
+            if (System.Text.RegularExpressions.Regex.IsMatch(input, @"\d{4}"))
+            {
+                if (!temp.ToString().Equals("1080"))
+                    return temp.Index;
+            }
+
+            return 0;
+        }
+
+        public int FindDateWithParentheses(string input)
+        {
+            var temp = System.Text.RegularExpressions.Regex.Match(input, @"\(\d{4}\)");
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(input, @"\(\d{4}\)"))
+            {
+                if (!temp.ToString().Equals("(1080)"))
+                    return temp.Index;
+            }
+
+            return 0;
+        }
+
+
+    }
+}
---
+++ FileNaming/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileNaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileNameUtility util = new FileNameUtility();
+
+            string path = Directory.GetCurrentDirectory();
+
+            Console.WriteLine("Enter the extension type");
+            string ext = Console.ReadLine();
+            
+            var files = Directory.GetFiles(path);
+                
+            foreach(var file in files)
+            {
+                string temp = file.Substring(path.Length + 1);
+
+                if (temp.Contains("File"))
+                {
+                    continue;
+                }
+
+                temp = temp.Replace('.', ' ');
+
+                int date = util.FindDate(temp);
+                    
+                //Date Found
+                if (date != 0)
+                {
+                    if ( util.FindDateWithParentheses(temp) != 0 )
+                    {
+                        var paren = util.FindDateWithParentheses(temp);
+                        temp = temp.Substring(0, (paren + 6));
+                    }
+                    else
+                    {
+                        string upToDate = temp.Substring(0, date);
+                        string dateString = temp.Substring(date, 4);
+                        string concat = upToDate + "(" + dateString + ")";
+                        temp = concat;
+                    }
+                        
+                }
+
+                Console.WriteLine(temp);
+                var newPath = path + "\\output\\" + temp + ext;
+                Directory.Move(file, newPath);
+            }
+
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
