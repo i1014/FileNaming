62a83d01d389a16d57486e41de5726799026ad1d
Ian Offerdahl <ianofferdahl@gmail.com>
2015-03-01 13:39:41 -0600

    Added ability to name television shows

--- FileNaming/FileNameUtility.cs
+++ FileNaming/FileNameUtility.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FileNaming
 {
     public class FileNameUtility
     {
+        public string CaptializeTitle(string input)
+        {
+            var result = Regex.Replace(input, @"\b(\w)", m => m.Value.ToUpper());
+            result = Regex.Replace(result, @"\s(of|in|by|and)\s", m => m.Value.ToLower(), RegexOptions.IgnoreCase);
+        
+            return result;
+        }
 
+        public int FindEpisode(string input)
+        {
+            var temp = System.Text.RegularExpressions.Regex.Match(input, @"(?i)s\d{2}e\d{2}");
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(input, @"(?i)s\d{2}e\d{2}"))
+            {
+                if (!temp.ToString().Equals("1080"))
+                    return temp.Index;
+            }
+
+            return 0;
+        }
 
         public int FindDate(string input)
         {
--- FileNaming/Program.cs
+++ FileNaming/Program.cs
@@ -17,10 +17,68 @@ namespace FileNaming
 
             Console.WriteLine("Enter the extension type");
             string ext = Console.ReadLine();
-            
+
+            Console.WriteLine("Menu:\n1) Movies\n2) Television");
+            string type = Console.ReadLine();
+            var t = Convert.ToInt32(type);
+
             var files = Directory.GetFiles(path);
-                
-            foreach(var file in files)
+
+            if(t == 1)
+                Movies(files, path, util,ext);
+
+            if (t == 2)
+                Television(files, path, util, ext);
+
+            
+        }
+
+        public static void Television(string[] files, string path, FileNameUtility util, string ext)
+        {
+            foreach (var file in files)
+            {
+                string temp = file.Substring(path.Length + 1);
+
+                if (temp.Contains("File"))
+                {
+                    continue;
+                }
+
+                temp = temp.Replace('.', ' ');
+
+                int episode = util.FindEpisode(temp);
+
+                //Episode Location
+                if (episode != 0)
+                {
+                    //Presumably the show.
+                    string upToEpisode = temp.Substring(0, episode);
+
+                    //Capitalize the show.
+                    upToEpisode = util.CaptializeTitle(upToEpisode);
+
+                    //Find the episode number.
+                    string episodeString = temp.Substring(episode, 6);
+                    episodeString = episodeString.ToUpper();
+
+                    //Put it all together.
+                    string concat = upToEpisode + episodeString;
+
+                    //Pass it along.
+                    temp = concat;
+                }
+
+                Console.WriteLine(temp);
+                var newPath = path + "\\output\\" + temp + ext;
+                Directory.Move(file, newPath);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Movies(string [] files, string path, FileNameUtility util, string ext)
+        {
+            foreach (var file in files)
             {
                 string temp = file.Substring(path.Length + 1);
 
@@ -32,11 +90,11 @@ namespace FileNaming
                 temp = temp.Replace('.', ' ');
 
                 int date = util.FindDate(temp);
-                    
+
                 //Date Found
                 if (date != 0)
                 {
-                    if ( util.FindDateWithParentheses(temp) != 0 )
+                    if (util.FindDateWithParentheses(temp) != 0)
                     {
                         var paren = util.FindDateWithParentheses(temp);
                         temp = temp.Substring(0, (paren + 6));
@@ -48,7 +106,7 @@ namespace FileNaming
                         string concat = upToDate + "(" + dateString + ")";
                         temp = concat;
                     }
-                        
+
                 }
 
                 Console.WriteLine(temp);
@@ -58,8 +116,6 @@ namespace FileNaming
 
             Console.ReadLine();
         }
-
-        
 
     }
 }
